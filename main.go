e22af03b1979e3c44153ed0e056c4906247c91e8
Jaina Srinath <jainasrinath@gmail.com>
2024-08-13 09:52:55 -0400

    Added a db.json file to retrieve the db configuration

--- main.go
+++ main.go
@@ -1,17 +1,61 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"hospitalmanagement/models"
 	"hospitalmanagement/pkg"
+	"io"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+type DBConfig struct {
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	Host      string `json:"host"`
+	Port      int    `json:"port"`
+	DBName    string `json:"dbname"`
+	Charset   string `json:"charset"`
+	ParseTime bool   `json:"parseTime"`
+	Loc       string `json:"loc"`
+}
+
+// LoadDBConfig loads the database configuration from a JSON file
+func LoadDBConfig() (*DBConfig, error) {
+	file, err := os.Open("db.json")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	byteValue, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var config DBConfig
+	if err := json.Unmarshal(byteValue, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
 func main() {
-	dsn := "root:password@tcp(127.0.0.1:3306)/hospitaldb?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+    config, err := LoadDBConfig()
+    if err != nil {
+        log.Fatal("Failed to load database configuration:", err)
+    }
+
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=%t&loc=%s",
+    config.Username, config.Password, config.Host, config.Port,
+    config.DBName, config.Charset, config.ParseTime, config.Loc)	
+    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
